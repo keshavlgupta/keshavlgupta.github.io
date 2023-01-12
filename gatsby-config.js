d804590437bf2bc346ed411c870c48358797bb96
Keshav Gupta <33162944+Kggupta@users.noreply.github.com>
2023-01-12 16:07:26 -0500

    Updated gatsby configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,29 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
+const conf = require('./config.json')
 
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
+  pathPrefix: `/`,
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: conf.title,
+    description: conf.description,
+    author: conf.author,
+    keywords: conf.keywords,
+    image: conf.siteImage,
   },
   plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: conf.name,
+        short_name: conf.name,
+        start_url: `/`,
+        background_color: conf.background_color,
+        theme_color: conf.theme_color,
+        display: `standalone`,
+        icon: `public/favicon.png`,
+      }
+    },
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -24,20 +33,25 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    `gatsby-plugin-gatsby-cloud`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        trackingIds: [
+          conf.googleAnalytics
+        ],
+        gtagConfig: {
+          cookie_expires: 0,
+          send_page_view: true
+        },
+        pluginConfig: {
+          head: true,
+          respectDNT: true,
+        },
       },
     },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
 }
