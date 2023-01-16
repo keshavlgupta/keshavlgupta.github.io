92e24b2882c66b0c2b0c9ef6b34fccf596332c93
Keshav Gupta <33162944+Kggupta@users.noreply.github.com>
2023-01-15 19:45:14 -0500

    updated 404 page styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,44 @@
-import * as React from "react"
-
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import React from 'react';
+import Seo from "../components/seo";
+import { Helmet } from "react-helmet";
+import SplashScreen from '../components/SplashScreen';
 
 const NotFoundPage = () => (
-  <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+<div className="not-found">
+        <Helmet htmlAttributes={{itemtype: 'notfound'}} >
+            <style>{`
+                h1 {
+                    font-family: 'Segoe UI Light';
+                    font-weight: 300;
+                    color: #fff;
+                }
+                
+                html[itemtype='notfound'] body {
+                    font-family: 'Segoe UI';
+                    color: #fff;
+                    background: #000000;
+                    margin: 0;
+                }
+                
+                .not-found main {
+                    margin: 4% 10%;
+                }
+                
+                .emotion {
+                    font-size: 8em;
+                    margin: 0;
+                }
+            `}</style>
+        </Helmet>
+		<Seo title={"Not Found"}/>
+        <main>
+			<SplashScreen/>
+            <header>
+                <h1 className="emotion">404 Not Found</h1>
+            </header>
+            <p>Hey, who turned out the lights? </p><a rel="noreferrer" href="https://www.youtube.com/watch?v=mI7ZpwpdgXs" target="_blank"><p>(Doctor Who S04:E09)</p></a>
+        </main>
+      </div>
 )
-
-export const Head = () => <Seo title="404: Not Found" />
 
 export default NotFoundPage
