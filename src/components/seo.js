9e7bb4170113b461b40ff719f6745bf234c94599
Keshav Gupta <33162944+Kggupta@users.noreply.github.com>
2023-01-15 19:47:58 -0500

    config for search engine optimization

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,45 +1,124 @@
 /**
  * SEO component that queries for data with
- * Gatsby's useStaticQuery React hook
+ *  Gatsby's useStaticQuery React hook
  *
- * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-
-function Seo({ description, title, children }) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
-        }
-      }
-    `
-  )
-
-  const metaDescription = description || site.siteMetadata.description
-  const defaultTitle = site.siteMetadata?.title
-
-  return (
-    <>
-      <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
-      <meta name="description" content={metaDescription} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={metaDescription} />
-      <meta property="og:type" content="website" />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:creator" content={site.siteMetadata?.author || ``} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={metaDescription} />
-      {children}
-    </>
-  )
-}
-
-export default Seo
+ import * as React from "react"
+ import PropTypes from "prop-types"
+ import { Helmet } from "react-helmet"
+ import { useStaticQuery, graphql } from "gatsby"
+ import headshot from "../../static/images/keshav_gupta_profile_bg.png"
+ 
+ function Seo({ description, lang, meta, title }) {
+   const { site } = useStaticQuery(
+	 graphql`
+	   query {
+		 site {
+		   siteMetadata {
+			 title
+			 description
+			 author
+			 keywords
+		   }
+		 }
+	   }
+	 `
+   )
+ 
+   const metaDescription = description || site.siteMetadata.description
+   const defaultTitle = site.siteMetadata?.title
+ 
+   return (
+	 <Helmet
+	   htmlAttributes={{
+		 lang,
+	   }}
+	   title={title}
+	   titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
+	   meta={[
+		 {
+		   name: `description`,
+		   content: metaDescription,
+		 },
+		 {
+		   property: `og:title`,
+		   content: title,
+		 },
+		 {
+		   property: `og:description`,
+		   content: metaDescription,
+		 },
+		 {
+		   property: `og:type`,
+		   content: `website`,
+		 },
+		 {
+		   name: `twitter:card`,
+		   content: `summary`,
+		 },
+		 {
+		   name: `twitter:creator`,
+		   content: site.siteMetadata?.author || ``,
+		 },
+		 {
+		   name: `twitter:title`,
+		   content: title,
+		 },
+		 {
+		   name: `twitter:description`,
+		   content: metaDescription,
+		 },
+		 {
+		   name: "keywords",
+		   content: site.siteMetadata.keywords,
+		 },
+		 {
+		   name: "robots",
+		   content: "max-snippet:-1",
+		 }
+	   ]
+	   .concat(
+		[
+			   {
+				 property: "og:image",
+				 content: headshot,
+			   },
+			   {
+				 property: "og:image:width",
+				 content: headshot.width,
+			   },
+			   {
+				 property: "og:image:height",
+				 content: headshot.height,
+			   },
+			   {
+				 name: "twitter:card",
+				 content: "summary_large_headshot",
+			   },
+			 ]
+	   ).concat(meta)}
+	 />
+   )
+ }
+ 
+ Seo.defaultProps = {
+   lang: `en`,
+   meta: [],
+   description: ``,
+ }
+ 
+ Seo.propTypes = {
+   description: PropTypes.string,
+   lang: PropTypes.string,
+   meta: PropTypes.arrayOf(PropTypes.object),
+   title: PropTypes.string.isRequired,
+   headshot: PropTypes.shape({
+	 src: PropTypes.string.isRequired,
+	 height: PropTypes.number.isRequired,
+	 width: PropTypes.number.isRequired,
+   }),
+ }
+ 
+ export default Seo
