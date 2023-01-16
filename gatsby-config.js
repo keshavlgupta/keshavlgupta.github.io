0eec175101c0871a61a9136ba010304de4353c59
Keshav Gupta <33162944+Kggupta@users.noreply.github.com>
2023-01-15 19:43:42 -0500

    updated static images folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/static/images`,
       },
     },
     `gatsby-transformer-sharp`,
