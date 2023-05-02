3952b886519a8a4743427f683b52afc2dcd4ee3c
Keshav Gupta <33162944+Kggupta@users.noreply.github.com>
2023-05-02 18:35:27 -0400

    Updates

---
+++ projects 10.js
@@ -0,0 +1,61 @@
+const projects = [
+	{
+		image: "./images/projects/keshav_gupta_stock_market_bot.png",
+		title: "Big Brother",
+		details: `A containerized bot hosted in AWS using GoLang, Express.js, MongoDB, Node.js to analyze trends in a stock market game, currently serving 50,000+ daily users and supported on Patreon.`,
+		source: "https://github.com/Kggupta/BigBrother-Demo",
+		website: "https://www.patreon.com/bigbrotherbot"
+	},
+	{
+	 	image: "./images/projects/keshav_gupta_au_tomato_language.png",
+	 	title: "AuTomato",
+	 	details: `A programming language, featuring a lexer, parser, and interpreter using C++, object-oriented programming, 
+	 			and design patterns to execute code.`,
+	 	website: "",
+	 	source: "https://github.com/Kggupta/AuTomato",
+	},
+	{
+		image: "./images/favicon.png",
+		title: "Portfolio Website",
+		details: `My personal website, built using TypeScript, JavaScript, and React. Integrated Search Engine Optimization (SEO) using Gatsby.`,
+		website: "https://keshavlgupta.me/",
+		source: "https://github.com/Kggupta/Kggupta.github.io",
+	},
+	{
+	 	image: "./images/projects/keshav_gupta_workout_bird.jpeg",
+	 	title: "WorkoutBuddy",
+	 	details: `An iOS and WatchOS application that helps you track your workouts and fitness goals.`,
+	 	website: "",
+	 	source: "https://github.com/Kggupta/WorkoutBuddy",
+	},
+	//{
+	// 	image: "./images/projects/keshav_gupta_docstealth.png",
+	// 	title: "DocStealth",
+	// 	details: `A CLI tool built with GoLang to edit and remove metadata from documents and images in a folder.`,
+	// 	website: "",
+	//	source: "https://github.com/Kggupta/DocStealth",
+	//},
+	{
+	 	image: "./images/projects/keshav_gupta_shoppies_shopify.png",
+		title: "The Shoppies",
+		details: `My attempt at the shopify developer challenge allowing users to nominate movies via the OMDB API.`,
+		website: "https://shopifyshoppies.web.app/",
+		source: "https://github.com/Kggupta/ShopifyShoppies",
+	},
+	{
+		image: "./images/projects/keshav_gupta_mips_emulator.png",
+		title: "Mips Emulator",
+		details: `A program that consumes MIPS assembly code, runs the code, and outputs the final state of the environment using C++ and Make.`,
+		website: "",
+		source: "https://github.com/Kggupta/MipsEmulator",
+	},
+	{
+		image: "./images/projects/keshav_gupta_google_api_project.png",
+		title: "Bungee",
+		details: `React Native application to manage a monthly budget and display spending statistics with a REST back-end leveraging Google APIs and Google Cloud.`,
+		website: "",
+		source: "https://github.com/Kggupta/Bungee",
+	}
+];
+
+export default projects;
