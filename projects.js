198d453f98ef50d0b21a64f79332eb1055c056c2
Keshav Gupta <33162944+Kggupta@users.noreply.github.com>
2024-12-01 15:17:37 -0500

    Updates

--- projects.js
+++ projects.js
@@ -1,47 +1,47 @@
 const projects = [
-	{
-		image: "./images/projects/keshav_gupta_stock_market_bot.png",
-		title: "Big Brother",
-		details: `A containerized bot hosted in AWS using GoLang, Express.js, MongoDB, Node.js to analyze trends in a stock market game, currently serving 50,000+ daily users and supported on Patreon.`,
-		source: "https://github.com/Kggupta/BigBrother-Demo",
-		website: "https://www.patreon.com/bigbrotherbot"
-	},
-	{
-	 	image: "./images/projects/keshav_gupta_au_tomato_language.png",
-	 	title: "AuTomato",
-	 	details: `A programming language, featuring a lexer, parser, and interpreter using C++, object-oriented programming, 
+  {
+    image: "./images/projects/keshav_gupta_stock_market_bot.png",
+    title: "Big Brother",
+    details: `A containerized bot hosted in AWS using GoLang, Express.js, MongoDB, Node.js to analyze trends in a stock market game, currently serving 50,000+ daily users and supported on Patreon.`,
+    source: "https://github.com/keshavlgupta/BigBrother-Demo",
+    website: "https://www.patreon.com/bigbrotherbot",
+  },
+  {
+    image: "./images/projects/keshav_gupta_au_tomato_language.png",
+    title: "AuTomato",
+    details: `A programming language, featuring a lexer, parser, and interpreter using C++, object-oriented programming, 
 	 			and design patterns to execute code.`,
-	 	website: "",
-	 	source: "https://github.com/Kggupta/AuTomato",
-	},
-	{
-		image: "./images/favicon.png",
-		title: "Portfolio Website",
-		details: `My personal website, built using TypeScript, JavaScript, and React. Integrated Search Engine Optimization (SEO) using Gatsby.`,
-		website: "https://kggupta.github.io",
-		source: "https://github.com/Kggupta/Kggupta.github.io",
-	},
-	{
-	 	image: "./images/projects/keshav_gupta_workout_bird.jpeg",
-	 	title: "WorkoutBuddy",
-	 	details: `An iOS and WatchOS application that helps you track your workouts and fitness goals.`,
-	 	website: "",
-	 	source: "https://github.com/Kggupta/WorkoutBuddy",
-	},
-	{
-		image: "./images/projects/keshav_gupta_mips_emulator.png",
-		title: "Mips Emulator",
-		details: `A program that consumes MIPS assembly code, runs the code, and outputs the final state of the environment using C++ and Make.`,
-		website: "",
-		source: "https://github.com/Kggupta/MipsEmulator",
-	},
-	{
-		image: "./images/projects/keshav_gupta_google_api_project.png",
-		title: "Bungee",
-		details: `React Native application to manage a monthly budget and display spending statistics with a REST back-end leveraging Google APIs and Google Cloud.`,
-		website: "",
-		source: "https://github.com/Kggupta/Bungee",
-	}
-];
+    website: "",
+    source: "https://github.com/keshavlgupta/AuTomato",
+  },
+  {
+    image: "./images/favicon.png",
+    title: "Portfolio Website",
+    details: `My personal website, built using TypeScript, JavaScript, and React. Integrated Search Engine Optimization (SEO) using Gatsby.`,
+    website: "https://keshavlgupta.github.io",
+    source: "https://github.com/keshavlgupta/keshavlgupta.github.io",
+  },
+  {
+    image: "./images/projects/keshav_gupta_workout_bird.jpeg",
+    title: "WorkoutBuddy",
+    details: `An iOS and WatchOS application that helps you track your workouts and fitness goals.`,
+    website: "",
+    source: "https://github.com/keshavlgupta/WorkoutBuddy",
+  },
+  {
+    image: "./images/projects/keshav_gupta_mips_emulator.png",
+    title: "Mips Emulator",
+    details: `A program that consumes MIPS assembly code, runs the code, and outputs the final state of the environment using C++ and Make.`,
+    website: "",
+    source: "https://github.com/keshavlgupta/MipsEmulator",
+  },
+  {
+    image: "./images/projects/keshav_gupta_google_api_project.png",
+    title: "Bungee",
+    details: `React Native application to manage a monthly budget and display spending statistics with a REST back-end leveraging Google APIs and Google Cloud.`,
+    website: "",
+    source: "https://github.com/keshavlgupta/Bungee",
+  },
+]
 
-export default projects;
+export default projects
